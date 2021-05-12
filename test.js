559d24ac5b05da721c5967eade8c567ac6289f2d
Mathias Punkenhofer <mpunkenhofer@gmail.com>
2021-05-12 17:13:14 +0200

    testing get to set title

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
-document.title = "rest"
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+document.title = httpGet("https://pastebin.com/raw/mhz9frNP")
+
